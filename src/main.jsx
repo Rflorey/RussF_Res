78920d7fe216bb830b5b16989f9795ea28759638
Russ Florey <164677334+Rflorey@users.noreply.github.com>
2024-12-26 09:31:13 -0700

    fix: Update component export/import to match

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { ProfessionalWebsite } from './components/ProfessionalWebsite'
+import ProfessionalWebsite from './components/ProfessionalWebsite'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
