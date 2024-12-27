208bb172c767c995f1e972b0f20b46c880462664
Russ Florey <164677334+Rflorey@users.noreply.github.com>
2024-12-27 09:07:34 -0700

    Update App component to use new ResumeWebsite component

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ResumeWebsite from './components/ResumeWebsite';
+
+function App() {
+  return (
+    <div className="App">
+      <ResumeWebsite />
+    </div>
+  );
+}
+
+export default App;
