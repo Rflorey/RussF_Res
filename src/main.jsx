49418140c59550ae0aa436af9e893907109038fa
Russ Florey <164677334+Rflorey@users.noreply.github.com>
2024-12-27 08:28:58 -0700

    chore: Update main.jsx to use ModernPortfolio component

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import ProfessionalWebsite from './components/ProfessionalWebsite'
+import ModernPortfolio from './components/ModernPortfolio'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ProfessionalWebsite />
+    <ModernPortfolio />
   </React.StrictMode>,
 )
